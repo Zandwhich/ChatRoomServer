6a3d8a874f2eabdbea072a65f3e8fcf3dae9a9d6
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2020-04-16 17:39:25 -0700

    Setup so that I can test multithreading in the chat client

--- README.md
+++ README.md
@@ -2,3 +2,47 @@
 Server-Side Code of the Chat Room
 
 I am using json_simple. I didn't make it.
+
+Example of JSONs:
+
+To Server:
+{
+	"name": "Alex",
+	"message": "This is a test",
+	"time": 7894612543
+}
+
+From Server:
+{
+	"name": {
+		"text": "Alex",
+		"colour": {
+			"red": 30,
+			"green": 187,
+			"blue": 92
+		}
+	},
+	"message": {
+		"text": "This is the message",
+		"colour": {
+			"red": 30,
+			"green": 187,
+			"blue": 92
+		}
+	},
+	"time": 157951354513215
+}
+
+OR
+
+{
+	"messsage": {
+		"text": "Alex has joined the chat",
+		"colour": {
+			"red": 20,
+			"green": 78,
+			"blue": 197
+		}
+	},
+	"time": 15798715648684
+}
--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -2,6 +2,10 @@ package com.company;/*
  * Author: Alex Zdanowicz
  */
 
+import json_simple.JSONObject;
+import json_simple.parser.JSONParser;
+
+import java.awt.*;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -21,6 +25,46 @@ public class Controller {
      * The starting port number for the program
      */
     public static final int STARTING_PORT = 1024;
+
+    /**
+     * The key for the name part of the object
+     */
+    public static final String NAME_KEY = "name";
+
+    /**
+     * The key for the message part of the object
+     */
+    public static final String MESSAGE_KEY = "message";
+
+    /**
+     * The key for the text of objects
+     */
+    public static final String TEXT_KEY = "text";
+
+    /**
+     * The key for the time that is sent in
+     */
+    public static final String TIME_KEY = "time";
+
+    /**
+     * The key for the color object
+     */
+    public static final String COLOR_KEY = "color";
+
+    /**
+     * The key for the red value in the color
+     */
+    public static final String RED_KEY = "red";
+
+    /**
+     * The key for the green value in the color
+     */
+    public static final String GREEN_KEY = "green";
+
+    /**
+     * The key for the blue value in the color
+     */
+    public static final String BLUE_KEY = "blue";
 
     // Variables
 
@@ -64,7 +108,14 @@ public class Controller {
 
             // Test receiving messages
             while (true) {
-                System.out.println(this.participants.get(0).retrieveMessage());
+                // TODO: This is only here temporarily
+                String temp = this.participants.get(0).retrieveMessage();
+                System.out.println(temp);
+                JSONParser parser = new JSONParser();
+                JSONObject message = (JSONObject) parser.parse(temp);
+                JSONObject returnMessage = this.constructNamedMessage((String) message.get(Controller.NAME_KEY),
+                        (String) message.get(Controller.MESSAGE_KEY), Color.BLUE, Color.BLACK);
+                this.broadcast(returnMessage.toString());
             }//end while
 
 //            this.participants.get(0).sendMessage("This is a test");
@@ -90,8 +141,44 @@ public class Controller {
                         participant.getName());
                 System.err.println("Message: " + e.getMessage());
                 System.err.println("Cause: " + e.getCause());
-                System.err.println("Stack Trace: " + e.getStackTrace().toString());
+                System.err.println("Stack Trace:"); e.printStackTrace();
             }//end try/catch
         }//end for
     }//end broadcast()
+
+    /**
+     * Constructs the JSONObject for a named object (as according to the README)
+     * @param name The name of the participant that sent the message
+     * @param message The message of the participant
+     * @param nameColor The color of the name
+     * @return The constructed JSONObject with all of the information
+     */
+    @SuppressWarnings("DuplicatedCode")
+    public JSONObject constructNamedMessage(String name, String message, Color nameColor, Color messageColor) {
+        JSONObject nameObject = new JSONObject();
+        nameObject.put(Controller.TEXT_KEY, name);
+
+        JSONObject nameColorObject = new JSONObject();
+        nameColorObject.put(Controller.RED_KEY, nameColor.getRed());
+        nameColorObject.put(Controller.GREEN_KEY, nameColor.getGreen());
+        nameColorObject.put(Controller.BLUE_KEY, nameColor.getBlue());
+
+        nameObject.put(Controller.COLOR_KEY, nameColorObject);
+
+        JSONObject messageObject = new JSONObject();
+        messageObject.put(Controller.TEXT_KEY, message);
+
+        JSONObject messageColorObject = new JSONObject();
+        messageColorObject.put(Controller.RED_KEY, messageColor.getRed());
+        messageColorObject.put(Controller.GREEN_KEY, messageColor.getGreen());
+        messageColorObject.put(Controller.BLUE_KEY, messageColor.getBlue());
+
+        messageObject.put(Controller.COLOR_KEY, messageColorObject);
+
+        JSONObject namedMessageObject = new JSONObject();
+        namedMessageObject.put(Controller.NAME_KEY, nameObject);
+        namedMessageObject.put(Controller.MESSAGE_KEY, messageObject);
+
+        return namedMessageObject;
+    }//end constructNamedMessage()
 }//end com.company.Controller
